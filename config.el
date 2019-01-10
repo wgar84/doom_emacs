11d4ddd59115080a5e81906dfae8b78af1449ea6
Guilherme Garcia <pectinidae@gmail.com>
2019-01-10 17:35:47 -0200

    init

---
+++ config.el
@@ -0,0 +1,8 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(setq doom-font (font-spec :family "Fira Mono" :size 13))
+
+(set-frame-parameter (selected-frame) 'alpha '(80 . 65))
+
